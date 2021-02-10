5059022e6c4edcf219dc7091a687fbd683a55812
joe-re <joe.tialtngo@gmail.com>
2021-02-10 09:42:45 +0900

    implement change background

--- main.ts
+++ main.ts
@@ -3,7 +3,7 @@ import { SegmentationVideo } from './src/index'
 const startVideoButton = document.getElementById('start_video_button') as HTMLButtonElement
 const localVideo = document.getElementById('local_video') as HTMLVideoElement
 const maskedVideo = document.getElementById('masked_video') as HTMLVideoElement
-const canvas = document.getElementById('canvas') as HTMLCanvasElement
+const img = document.getElementById('image') as HTMLImageElement
 export async function startVideo() {
   const mediaConstraints = { video: { width: 640, height: 480 }, audio: false };
 
@@ -25,7 +25,14 @@ export async function startVideo() {
 async function startMaskedVideo() {
   const segmentationVideo = new SegmentationVideo()
   await segmentationVideo.init()
-  const stream = segmentationVideo.createMaskedStream(localVideo)
+  const canvas = document.createElement('canvas')
+  canvas.width = img.width
+  canvas.height = img.height
+  const ctx = canvas.getContext('2d')!
+  ctx.drawImage(img, 0, 0)
+  const imageData = ctx.getImageData(0, 0, img.width, img.height)
+  // const stream = segmentationVideo.createMaskedStream(localVideo)
+  const stream = segmentationVideo.createChangedBackgroundStream(localVideo, imageData)
   maskedVideo.srcObject = stream
   maskedVideo.play()
 }
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,5 @@
 import * as bodyPix from '@tensorflow-models/body-pix';
 import '@tensorflow/tfjs';
-import { createLoopVariable } from 'typescript';
 
 interface CanvasElement extends HTMLCanvasElement {
   captureStream(frameRate?: number): MediaStream;
@@ -40,12 +39,53 @@ export class SegmentationVideo {
     requestAnimationFrame(loop)
     return stream
   }
-  private async updateStream(src: HTMLVideoElement) {
-    const segmentation = await this.net!.segmentPerson(src)
-    const fgColor = { r: 0, g: 0, b: 0, a: 0 };
-    const bgColor = { r: 127, g: 127, b: 127, a: 255 };
-    const mask = bodyPix.toMask(segmentation, fgColor, bgColor)
-    bodyPix.drawMask(this.canvas, src, mask, 1.0, 3)
+  private async updateStream(src: HTMLVideoElement, imageData?: ImageData) {
+    const segmentation = await this.net!.segmentPerson(src, { maxDetections: 1 })
+    const fgColor = { r: 0, g: 0, b: 0, a: 0 }
+    const bgColor = { r: 127, g: 127, b: 127, a: 255 }
+    if (!imageData) {
+      const mask = bodyPix.toMask(segmentation, fgColor, bgColor)
+      bodyPix.drawMask(this.canvas, src, mask, 1)
+    } else {
+      const mask = this.transparent(imageData, segmentation)
+      bodyPix.drawMask(this.canvas, src, mask, 1)
+    }
+  }
+
+  createChangedBackgroundStream(src: HTMLVideoElement, imageData: ImageData) {
+    const stream = this.canvas.captureStream(30)
+    const loop = (() => {
+      this.updateStream(src, imageData)
+      requestAnimationFrame(loop)
+    })
+    requestAnimationFrame(loop)
+    return stream
+  }
+
+  private transparent(imageData: ImageData, segmentation: bodyPix.SemanticPersonSegmentation) {
+    let multiPersonSegmentation: Array<
+      bodyPix.SemanticPersonSegmentation |
+      bodyPix.PersonSegmentation
+    >
+
+    if (!Array.isArray(segmentation)) {
+      multiPersonSegmentation = [segmentation];
+    } else {
+      multiPersonSegmentation = segmentation;
+    }
+    const tranparented = new Uint8ClampedArray(imageData.data)
+    const { height, width } = segmentation
+    for (let i = 0; i < height; i++) {
+      for (let j = 0; j < width; j++) {
+        const n = i * width + j
+        for (let k = 0; k < multiPersonSegmentation.length; k++) {
+          if (multiPersonSegmentation[k].data[n]) {
+            tranparented[4 * n + 3] = 0
+          }
+        }
+      }
+    }
+    return new ImageData(tranparented, width, height)
   }
 }
 
