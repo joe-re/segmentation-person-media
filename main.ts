8386c5e0a3516d3f0c482226142a016ecf55ff5d
joe-re <joe.tialtngo@gmail.com>
2021-02-15 01:01:24 +0900

    refactor createStream on demo page

--- main.ts
+++ main.ts
@@ -11,7 +11,13 @@ const selectionBlur = document.getElementById('selection-blur') as HTMLInputElem
 
 let selection: 'mask' | 'change-background' | 'blur' = 'mask'
 
-export async function startVideo() {
+let initialized = false
+async function startVideo() {
+  if (initialized) {
+    playVideo()
+    return
+  }
+  const segmentationMedia = await load()
   const mediaConstraints = { video: { width: 640, height: 480 }, audio: false };
 
   const localStream = await navigator.mediaDevices.getUserMedia(mediaConstraints).catch(err => {
@@ -20,29 +26,40 @@ export async function startVideo() {
   }) as MediaStream
 
   localVideo.srcObject = localStream;
-  await localVideo.play().catch(err => console.error('failed to play local video:', err))
   localVideo.volume = 0
-  const segmentationMedia = await load()
-
-  let stream: MediaStream;
-  if (selection === 'mask') {
-    stream = segmentationMedia.createMaskedStream({ src: localVideo })
-  } else if (selection === 'change-background') {
-    const canvas = document.createElement('canvas')
-    canvas.width = img.width
-    canvas.height = img.height
-    const ctx = canvas.getContext('2d')!
-    ctx.drawImage(img, 0, 0)
-    const imageData = ctx.getImageData(0, 0, img.width, img.height)
-    stream = segmentationMedia.createChangedBackgroundStream({ src: localVideo, backgroundImage: imageData })
-  } else {
-    stream = segmentationMedia.createBluredStream({ src: localVideo })
+  localVideo.onloadeddata = () => {
+    localVideo.play().catch(err => console.error('failed to play local video:', err))
   }
-  maskedVideo.srcObject = stream
-  maskedVideo.play()
+
+  localVideo.onplay = () => {
+    let stream: MediaStream;
+    if (selection === 'mask') {
+      stream = segmentationMedia.createMaskedStream({ src: localVideo })
+    } else if (selection === 'change-background') {
+      const canvas = document.createElement('canvas')
+      canvas.width = img.width
+      canvas.height = img.height
+      const ctx = canvas.getContext('2d')!
+      ctx.drawImage(img, 0, 0)
+      const imageData = ctx.getImageData(0, 0, img.width, img.height)
+      stream = segmentationMedia.createChangedBackgroundStream({ src: localVideo, backgroundImage: imageData })
+    } else {
+      stream = segmentationMedia.createBluredStream({ src: localVideo })
+    }
+    maskedVideo.srcObject = stream
+    maskedVideo.play()
+  }
+  initialized = true
 }
 
-export async function pauseVideo() {
+function playVideo() {
+  if (!localVideo.paused) {
+    pauseVideo()
+  }
+  localVideo.play().catch(err => console.error('failed to play local video:', err))
+}
+
+async function pauseVideo() {
   localVideo.pause()
 }
 
